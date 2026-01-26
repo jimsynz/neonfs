71f5c419df15312522844d03727e7897d37ff987
James Harton <james@harton.nz>
2026-01-26 18:48:53 +1300

    chore: 🔥 initial commit 🔥

---
+++ lib/neon_fs.ex
@@ -0,0 +1,18 @@
+defmodule NeonFS do
+  @moduledoc """
+  Documentation for `NeonFS`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NeonFS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/neon_fs_test.exs
@@ -0,0 +1,8 @@
+defmodule NeonFSTest do
+  use ExUnit.Case
+  doctest NeonFS
+
+  test "greets the world" do
+    assert NeonFS.hello() == :world
+  end
+end
